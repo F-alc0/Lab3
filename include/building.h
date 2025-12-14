b8c61a7fc8f1dce575cd3401a2a2b3f91a2bfc6b
F-alc0 <oss19042007@yandex.ru>
2025-12-15 01:04:21 +0300

    Функции для работы со зданиями

--- include/building.h
+++ include/building.h
@@ -21,5 +21,12 @@ typedef struct {
     double avg_area;
 } Building;
 
+void print_building_help();
+void print_building_csv(const Building *bld, FILE *out);
+int read_building_csv(Building *bld, FILE *in);
+void print_building_table_header(FILE *out);
+void print_building_table_row(const Building *bld, FILE *out);
+void print_building_table_footer(FILE *out);
+void generate_random_building(Building *bld);
 
 #endif
