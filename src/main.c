77aae043dd50c4c117a03013dcf04b86cbc46fc1
F-alc0 <oss19042007@yandex.ru>
2025-12-16 00:59:55 +0300

    Реализация разных режимов работы

---
+++ src/main.c
@@ -0,0 +1,27 @@
+#include "cli.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc == 1) {
+        print_help();
+        return 0;
+    }
+    Config cfg = parse_args(argc, argv);
+    switch (cfg.mode) {
+        case MODE_GENERATE:
+            run_generate(cfg.N, cfg.output_file);
+            break;
+        case MODE_SORT:
+            run_sort(cfg.input_file, cfg.output_file, cfg.sort_type, cfg.sort_key);
+            break;
+        case MODE_PRINT:
+            run_print(cfg.input_file, cfg.output_file);
+            break;
+        default:
+            fprintf(stderr, "Ошибка: не указан режим работы\n");
+            print_help();
+            return 1;
+    }
+    return 0;
+}
