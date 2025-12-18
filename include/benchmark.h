00bbb1d9ef01480fdb43288c7130d944c4945bcc
F-alc0 <oss19042007@yandex.ru>
2025-12-18 12:54:54 +0300

    Сравнение сортировок

---
+++ include/benchmark.h
@@ -0,0 +1,21 @@
+#ifndef BENCHMARK_H
+#define BENCHMARK_H
+
+#include "vector.h"
+
+typedef void (*SortFunction)(Vector *vec,
+                             int (*cmp)(const void *, const void *));
+
+typedef struct {
+    int N;
+    double comb_time;
+    double quick_time;
+} BenchmarkResult;
+
+double benchmark_sort(SortFunction sort_func,
+                      Vector *vec,
+                      int (*cmp)(const void *, const void *));
+
+void run_benchmark(void);
+
+#endif
