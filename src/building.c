750f923756af09a7bbed189c9389d93149b70a3c
F-alc0 <oss19042007@yandex.ru>
2025-12-21 14:41:23 +0300

    Изменил C файл

--- src/building.c
+++ src/building.c
@@ -9,21 +9,21 @@ void print_building_help(void) {
     puts("  - Застройщик (строка)");
     puts("  - Микрорайон (строка)");
     puts("  - Тип дома (PANEL/BRICK/MONOLITH)");
-    puts("  - Год постройки (четырехзначное число)");
+    puts("  - Год постройки");
     puts("  - Наличие лифта (0/1)");
     puts("  - Наличие мусоропровода (0/1)");
-    puts("  - Количество квартир (целое число)");
-    puts("  - Количество этажей (целое число)");
-    puts("  - Средняя площадь квартиры (вещественное число)");
+    puts("  - Количество квартир");
+    puts("  - Количество этажей");
+    puts("  - Средняя площадь квартиры");
 }
 
 void print_building_csv(const Building *bld, FILE *out) {
-    const char *type_str;
+    const char *type_str = "UNKNOWN";
+
     switch (bld->type) {
-        case PANEL: type_str = "PANEL"; break;
-        case BRICK: type_str = "BRICK"; break;
+        case PANEL:    type_str = "PANEL"; break;
+        case BRICK:    type_str = "BRICK"; break;
         case MONOLITH: type_str = "MONOLITH"; break;
-        default: type_str = "UNKNOWN";
     }
     fprintf(out, "%s,%s,%s,%d,%d,%d,%d,%d,%.2f\n",
             bld->developer,
@@ -38,86 +38,96 @@ void print_building_csv(const Building *bld, FILE *out) {
 }
 
 int read_building_csv(Building *bld, FILE *in) {
-    char type_str[20];
+    char type_str[32];
     char line[512];
-    if (!fgets(line, sizeof(line), in)) {
+
+    if (!fgets(line, sizeof(line), in))
         return 0;
-    }
+
     line[strcspn(line, "\n")] = '\0';
-    int ret = sscanf(line, "%99[^,],%99[^,],%19[^,],%d,%d,%d,%d,%d,%lf",
-                     bld->developer,
-                     bld->district,
-                     type_str,
-                     &bld->year,
-                     &bld->has_elevator,
-                     &bld->has_chute,
-                     &bld->apartments,
-                     &bld->floors,
-                     &bld->avg_area);
-    if (ret != 9) {
+
+    int ret = sscanf(line,
+        "%99[^,],%99[^,],%31[^,],%hd,%hhu,%hhu,%hd,%hhu,%lf",
+        bld->developer,
+        bld->district,
+        type_str,
+        &bld->year,
+        &bld->has_elevator,
+        &bld->has_chute,
+        &bld->apartments,
+        &bld->floors,
+        &bld->avg_area
+    );
+
+    if (ret != 9)
         return 0;
-    }
-    if (strcmp(type_str, "PANEL") == 0) {
+
+    if (strcmp(type_str, "PANEL") == 0)
         bld->type = PANEL;
-    } else if (strcmp(type_str, "BRICK") == 0) {
+    else if (strcmp(type_str, "BRICK") == 0)
         bld->type = BRICK;
-    } else if (strcmp(type_str, "MONOLITH") == 0) {
+    else if (strcmp(type_str, "MONOLITH") == 0)
         bld->type = MONOLITH;
-    } else {
+    else
         return 0;
-    }
+
     return 1;
 }
 void print_building_table_header(FILE *out) {
-    fprintf(out, "+----------------------+-----------------+-----------------+------+----------+--------------+---------------+--------------+-----------------+\n");
-    fprintf(out, "| Застройщик           | Микрорайон      | Тип дома        | Год  | Лифт     | Мусоропровод | Всего квартир | Всего этажей | Средняя площадь |\n");
-    fprintf(out, "+----------------------+-----------------+-----------------+------+----------+--------------+---------------+--------------+-----------------+\n");
+    fprintf(out,
+        "+----------------------+-----------------+-----------------+------+----------+--------------+---------------+--------------+-----------------+\n"
+        "| Застройщик           | Микрорайон      | Тип дома        | Год  | Лифт     | Мусоропровод | Всего квартир | Всего этажей | Средняя площадь |\n"
+        "+----------------------+-----------------+-----------------+------+----------+--------------+---------------+--------------+-----------------+\n"
+    );
 }
 
 void print_building_table_row(const Building *bld, FILE *out) {
-    const char *type_str;
+    const char *type_str = "Неизвестно";
+
     switch (bld->type) {
-        case PANEL: type_str = "Панельный"; break;
-        case BRICK: type_str = "Кирпичный"; break;
+        case PANEL:    type_str = "Панельный"; break;
+        case BRICK:    type_str = "Кирпичный"; break;
         case MONOLITH: type_str = "Монолитный"; break;
-        default: type_str = "Неизвестно";
     }
-    const char *elevator = bld->has_elevator ? "ДА" : "НЕТ";
-    const char *chute = bld->has_chute ? "ДА" : "НЕТ";
 
     fprintf(out,
-            "| %-20s | %-22s | %-18s | %4d | %-10s | %-13s | %-11d | %-6d | %-16.2f |\n",
-            bld->developer,
-            bld->district,
-            type_str,
-            bld->year,
-            elevator,
-            chute,
-            bld->apartments,
-            bld->floors,
-            bld->avg_area);
+        "| %-20s | %-15s | %-15s | %4d | %-8s | %-12s | %-13d | %-12d | %-15.2f |\n",
+        bld->developer,
+        bld->district,
+        type_str,
+        bld->year,
+        bld->has_elevator ? "ДА" : "НЕТ",
+        bld->has_chute ? "ДА" : "НЕТ",
+        bld->apartments,
+        bld->floors,
+        bld->avg_area
+    );
 }
 
 void print_building_table_footer(FILE *out) {
-    fprintf(out, "+----------------------+-----------------+-----------------+------+----------+-------------+-------------+--------+----------------+\n");
+    fprintf(out,
+        "+----------------------+-----------------+-----------------+------+----------+--------------+---------------+--------------+-----------------+\n"
+    );
 }
 
 void generate_random_building(Building *bld) {
-    const char *developers[] = {
-        "Rockstar Estates", "TerraGroup Villas", "Lucas Constructions",
-        "CD Project Heights", "City 17", "Larian Builders",
-        "Stoic Housing", "Bethesda Just Works", "EAchitect"
-    };
-    const char *districts[] = {
-        "Верхний Ярнам", "Район №9", "Кейнхёрст",
-        "Олачиль", "Анор Лондо", "Иритилл",
-        "Трущобы", "Сайсил", "Аркс"
-    };
-    BuildingType types[] = {PANEL, BRICK, MONOLITH};
-    strcpy_s(bld->developer, sizeof(bld->developer), developers[rand() % 9]);
-    strcpy_s(bld->district, sizeof(bld->district), districts[rand() % 9]);
-    bld->type = types[rand() % 3];
-    bld->year = 1970 + rand() % 50;
+    if (!bld) return;
+
+    strcpy_s(
+        bld->developer,
+        sizeof(bld->developer),
+        BUILDING_DEVELOPERS[rand() % BUILDING_DEVELOPERS_COUNT]
+    );
+
+    strcpy_s(
+        bld->district,
+        sizeof(bld->district),
+        BUILDING_DISTRICTS[rand() % BUILDING_DISTRICTS_COUNT]
+    );
+
+    bld->type = (BuildingType)(rand() % 3);
+
+    bld->year = 1950 + rand() % 50;
     bld->has_elevator = rand() % 2;
     bld->has_chute = rand() % 2;
     bld->apartments = 20 + rand() % 300;
@@ -126,51 +136,55 @@ void generate_random_building(Building *bld) {
 }
 
 int compare_by_developer_asc(const void *a, const void *b) {
-    const Building *b1 = (const Building *)a;
-    const Building *b2 = (const Building *)b;
-    return strcmp(b1->developer, b2->developer);
+    return strcmp(
+        ((const Building *)a)->developer,
+        ((const Building *)b)->developer
+    );
 }
 
 int compare_by_district_asc(const void *a, const void *b) {
-    const Building *b1 = (const Building *)a;
-    const Building *b2 = (const Building *)b;
-    return strcmp(b1->district, b2->district);
+    return strcmp(
+        ((const Building *)a)->district,
+        ((const Building *)b)->district
+    );
 }
 
 int compare_by_year_asc(const void *a, const void *b) {
-    const Building *b1 = (const Building *)a;
-    const Building *b2 = (const Building *)b;
-    return (b1->year > b2->year) - (b1->year < b2->year);
+    const Building *ba = a;
+    const Building *bb = b;
+    return (ba->year > bb->year) - (ba->year < bb->year);
 }
 
 int compare_by_apartments_asc(const void *a, const void *b) {
-    const Building *b1 = (const Building *)a;
-    const Building *b2 = (const Building *)b;
-    return (b1->apartments > b2->apartments) - (b1->apartments < b2->apartments);
+    const Building *ba = a;
+    const Building *bb = b;
+    return (ba->apartments > bb->apartments) - (ba->apartments < bb->apartments);
 }
 
 int compare_by_floors_asc(const void *a, const void *b) {
-    const Building *b1 = (const Building *)a;
-    const Building *b2 = (const Building *)b;
-    return (b1->floors > b2->floors) - (b1->floors < b2->floors);
+    const Building *ba = a;
+    const Building *bb = b;
+    return (ba->floors > bb->floors) - (ba->floors < bb->floors);
 }
 
 int compare_by_avg_area_asc(const void *a, const void *b) {
-    const Building *b1 = (const Building *)a;
-    const Building *b2 = (const Building *)b;
-    return (b1->avg_area > b2->avg_area) - (b1->avg_area < b2->avg_area);
+    const Building *ba = a;
+    const Building *bb = b;
+    return (ba->avg_area > bb->avg_area) - (ba->avg_area < bb->avg_area);
 }
 
 int compare_by_has_elevator_asc(const void *a, const void *b) {
-    const Building *b1 = (const Building *)a;
-    const Building *b2 = (const Building *)b;
-    return (b1->has_elevator > b2->has_elevator) - (b1->has_elevator < b2->has_elevator);
+    const Building *ba = a;
+    const Building *bb = b;
+    return (ba->has_elevator > bb->has_elevator) -
+           (ba->has_elevator < bb->has_elevator);
 }
 
 int compare_by_has_chute_asc(const void *a, const void *b) {
-    const Building *b1 = (const Building *)a;
-    const Building *b2 = (const Building *)b;
-    return (b1->has_chute > b2->has_chute) - (b1->has_chute < b2->has_chute);
+    const Building *ba = a;
+    const Building *bb = b;
+    return (ba->has_chute > bb->has_chute) -
+           (ba->has_chute < bb->has_chute);
 }
 
 int compare_by_developer_desc(const void *a, const void *b) {
