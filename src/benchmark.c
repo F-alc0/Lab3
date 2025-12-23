934a97ffa4b70af07fa7e0038df02b2275243c7a
F-alc0 <oss19042007@yandex.ru>
2025-12-23 15:46:25 +0300

    Заработало

--- src/benchmark.c
+++ src/benchmark.c
@@ -80,8 +80,7 @@ static double benchmark_average(
 
 void run_benchmark(void) {
     const int sizes[] = {
-        5, 10, 20, 50, 100, 200, 500, 1000, 2000,
-        5000, 10000, 20000, 50000, 100000, 20000, 50000, 1000000
+        5, 10, 20, 50, 100, 200, 500
     };
 
     const int count = sizeof(sizes) / sizeof(sizes[0]);
